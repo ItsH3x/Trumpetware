e9fc3cbe3ebc1f4bc6a6d351bd1c8b3e88dacad7
Héctor Caro <h3x@H3x-PC.168.1.7>
2025-05-26 21:58:33 -0700

    Se agregaron herecias a la clase cuenta para diferenciar los tipos de cuenta

---
+++ ProyectoCajeroATM/CuentaAhorro.java
@@ -0,0 +1,27 @@
+package ProyectoCajeroATM;
+
+public class CuentaAhorro extends Cuenta {
+    private double tasaInteres;
+
+    public CuentaAhorro(int numero_de_cuenta, Cliente cliente, double tasaInteres) {
+        super(numero_de_cuenta, cliente);
+        this.tasaInteres = tasaInteres;
+    }
+
+    public double getTasaInteres() {
+        return tasaInteres;
+    }
+
+    public void setTasaInteres(double tasaInteres) {
+        this.tasaInteres = tasaInteres;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaAhorro{" +
+                "numero_de_cuenta=" + getNumero_de_cuenta() +
+                ", cliente=" + (getCliente() != null ? getCliente().toString() : "null") +
+                ", tasaInteres=" + tasaInteres +
+                '}';
+    }
+}
