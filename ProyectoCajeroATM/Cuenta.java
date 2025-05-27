7f1cd0e21175c1d5805594b5dcabff3322d7b333
Sebastian1247 <sebaspro3000@gmail.com>
2025-05-26 21:42:48 -0700

    Agregado atributo saldo a la clase Cuenta.java

--- ProyectoCajeroATM/Cuenta.java
+++ ProyectoCajeroATM/Cuenta.java
@@ -3,10 +3,12 @@ package ProyectoCajeroATM;
 public abstract class Cuenta {
     private int numero_de_cuenta;
     private Cliente cliente;
+    private double saldo;
 
-    public Cuenta(int numero_de_cuenta, Cliente cliente) {
+    public Cuenta(int numero_de_cuenta, Cliente cliente, double saldo) {
         this.numero_de_cuenta = numero_de_cuenta;
         this.cliente = cliente;
+        this.saldo = saldo;
     }
 
     public int getNumero_de_cuenta() {
@@ -25,11 +27,20 @@ public abstract class Cuenta {
         this.cliente = cliente;
     }
 
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
     @Override
     public String toString() {
         return "Cuenta{" +
                 "numero_de_cuenta=" + numero_de_cuenta +
                 ", cliente=" + (cliente != null ? cliente.toString() : "null") +
+                ", saldo=" + saldo +
                 '}';
     }
 }
